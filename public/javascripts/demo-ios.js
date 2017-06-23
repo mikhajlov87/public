ec524efe3feb4b1ec0fa59f8553cf5eed22fae37
Sergey <mikhajlov87@gmail.com>
2017-06-23 14:13:41 +0300

    rewrite carousel

--- public/javascripts/demo-ios.js
+++ public/javascripts/demo-ios.js
@@ -1,85 +1,106 @@
-$('.launch-btn').on('click', function () {
-    var display = $('.display-demo'),
-        clock = $('.clock-container');
-    display.toggleClass('on');
-    if ( display.hasClass('on') ) {
-        display.fadeIn();
-        clock.fadeIn();
-        $('.icon-ios').on('click', function () {
-            $(this).animate({
-                width: '100%',
-                height: '100%',
-                top: 0,
-                left: 0
-            }, 300);
-            $('.clock-container').fadeOut();
-            $(this).css({
-                'background': '#f0f0f0',
-                'border': 'none',
-                'cursor': 'default'
+$(document).ready(function () {
+    var carousel = $('.owl-carousel');
+
+    $('.launch-btn').on('click', function () {
+        var display = $('.display-demo'),
+            clock = $('.clock-container');
+        display.toggleClass('on');
+        if ( display.hasClass('on') ) {
+            display.fadeIn();
+            clock.fadeIn();
+            $('.icon-ios').on('click', function () {
+                $(this).animate({
+                    width: '100%',
+                    height: '100%',
+                    top: 0,
+                    left: 0
+                }, 300);
+                $('.clock-container').fadeOut();
+                $(this).css({
+                    'background': '#f0f0f0',
+                    'border': 'none',
+                    'cursor': 'default'
+                });
+                $('.app').fadeIn();
+                $(this).unbind('click');
             });
-            $('.app').fadeIn();
-            $(this).unbind('click');
+            carousel.owlCarousel({
+                margin: 0,
+                loop: true,
+                items: 1
+            });
+        } else {
+            clock.fadeOut();
+            $('.app').fadeOut();
+            $('.icon-ios').animate({
+                width: '50px',
+                height: '50px',
+                bottom: '30px',
+                left: '20px'
+            }, 300).css({
+                cursor: '',
+                width: '',
+                height: '',
+                top: '',
+                bottom: '',
+                left: '',
+                background: ''
+            });
+            setTimeout(function () {
+                display.fadeOut();
+            }, 150);
+        }
+    });
+
+    setInterval( function () {
+        var date = new Date(),
+            hours = $('.hours'),
+            minutes = $('.min'),
+            dateContainer = $('.date-container'),
+            options = {
+                month: 'long',
+                day: 'numeric',
+                weekday: 'long',
+                timezone: 'UTC'
+            };
+
+        if ( date.getHours() !== Number( hours.text() )) {
+            ( date.getHours() < 10 ) ? hours.text( '0' + date.getHours() ) : hours.text( date.getHours() );
+        }
+        if ( date.getMinutes() !== Number( minutes.text() )) {
+            ( date.getMinutes() < 10 ) ? minutes.text( '0' + date.getMinutes() ) : minutes.text( date.getMinutes() );
+        }
+        if ( date.toLocaleString('ru', options) !== dateContainer.text() ) {
+            dateContainer.text( date.toLocaleString('ru', options) );
+        }
+    }, 1000 );
+
+    carousel.find('img').mousedown(function (event) {
+        var carousel = $('.owl-carousel'),
+            imgContainer = $('.image-click-container'),
+            value = $(this).attr('src');
+
+        var start = event.pageX, end;
+
+        $(this).mouseup(function (event) {
+            end = event.pageX;
+            if (start === end) {
+                $('.static-panel').hide();
+                carousel.hide();
+                imgContainer.fadeIn();
+                imgContainer.find('img').attr('src', value).animate({ top: '180px' });
+            }
         });
-    } else {
-        clock.fadeOut();
-        $('.app').fadeOut();
-        $('.icon-ios').animate({
-            width: '50px',
-            height: '50px',
-            bottom: '30px',
-            left: '20px'
-        }, 300).css({
-            cursor: '',
-            width: '',
-            height: '',
-            top: '',
-            bottom: '',
-            left: '',
-            background: ''
-        });
+    });
+
+    $('.image-click-container').on('click', function () {
+        var that = $(this);
+
+        that.find('img').animate({ top: '20px' });
+        $('.static-panel').show();
         setTimeout(function () {
-            display.fadeOut();
-        }, 150);
-    }
-});
-
-setInterval( function () {
-    var date = new Date(),
-        hours = $('.hours'),
-        minutes = $('.min'),
-        dateContainer = $('.date-container'),
-        options = {
-            month: 'long',
-            day: 'numeric',
-            weekday: 'long',
-            timezone: 'UTC'
-        };
-
-    if ( date.getHours() !== Number( hours.text() )) {
-        ( date.getHours() < 10 ) ? hours.text( '0' + date.getHours() ) : hours.text( date.getHours() );
-    }
-    if ( date.getMinutes() !== Number( minutes.text() )) {
-        ( date.getMinutes() < 10 ) ? minutes.text( '0' + date.getMinutes() ) : minutes.text( date.getMinutes() );
-    }
-    if ( date.toLocaleString('ru', options) !== dateContainer.text() ) {
-        dateContainer.text( date.toLocaleString('ru', options) );
-    }
-}, 1000 );
-
-$('#carousel-example-generic').find('img').on('click', function () {
-    var carousel = $('#carousel-example-generic'),
-        imgContainer = $('.image-click-container'),
-        value = $(this).attr('src');
-
-    $('.static-panel').fadeOut();
-    carousel.fadeOut();
-    imgContainer.fadeIn();
-    imgContainer.find('img').attr('src', value);
-});
-
-$('.image-click-container').on('click', function () {
-    $(this).fadeOut();
-    $('.static-panel').fadeIn();
-    $('#carousel-example-generic').fadeIn();
+            that.fadeOut();
+            $('.owl-carousel').show();
+        }, 250);
+    });
 });
