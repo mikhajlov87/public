b564c6d9cd5c37c670003fd3b65ad01096a5c8ac
Sergey <mikhajlov87@gmail.com>
2017-07-18 15:57:13 +0300

    rewrite social-sign

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -13470,9 +13470,12 @@ $(document).ready(function () {
     }, 1000);
 
     logOut.on('click', function (event) {
+        var _this = $(this);
         event.preventDefault();
         profile.animate({'right': '-180px' }, 500);
-        window.location.href = $(this).attr("href");
+        setTimeout(function () {
+            window.location.href = _this.attr("href");
+        }, 500);
     });
 });
 $('.panel.rubrificator__panel').on('click', function () {
